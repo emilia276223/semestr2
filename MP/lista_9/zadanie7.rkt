ab73d537df3b00e5f24e6ebccc42e9af1dc643d3
Emilia <emilia.a.wis@gmail.com>
2023-05-08 22:08:39 +0200

    zadanie 7

--- MP/lista_9/zadanie7.rkt
+++ MP/lista_9/zadanie7.rkt
@@ -18,29 +18,35 @@
 
 (define (parse-exp0 ss)
   (type-case (ParseResult Exp) (parse-exp1 ss) 
-    [(parse-err) (parse-err)] ; jesli jest puste albo coś nie dziala ???
+    [(parse-err) (parse-err)]
     [(parse-ok e1 rest)
      (type-case (ParseResult Op) (parse-op0 rest)
-       [(parse-err) (parse-ok e1 null)] ;jesli jedno wyrazenie (konczy sie na liczbie mp)
+       [(parse-err)         (parse-ok e1 rest)]
        [(parse-ok op rest2)
         (type-case (ParseResult Exp) (parse-exp0 rest2)
-          [(parse-err) (parse-err)] ;jesli jest operator i nie ma za nim liczby / wyrazenia
-          [(parse-ok value rest) (parse-ok (exp-op e1 op value) null)])])])) ; bo 
+          [(parse-err) (parse-err)]
+          [(parse-ok e2 rest3) (parse-ok (exp-op op e1 e2) rest3)])])]))
 
 (define (parse-exp1 ss)
   (type-case (Listof S-Exp) ss
     [empty (parse-err)]
     [(cons s rest)
      (cond
-       [(s-exp-number? s) (parse-ok (exp-number (s-exp->number s)) rest)] ;jesli mamy liczbe
+       [(s-exp-number? s) (parse-ok (exp-number (s-exp->number s)) rest)]
        [(s-exp-list? s)
-        (type-case (ParseResult Exp) (parse-exp0 (s-exp->list s)) ;jesli mamy wyrazenie
+        (type-case (ParseResult Exp) (parse-exp0 (s-exp->list s))
           [(parse-err) (parse-err)]
-          [(parse-ok e rest2) (parse-ok e rest)])])]));rest2 jest puste z zalozenia, wiec nie trzeba if-owac
+          [(parse-ok e rest2)
+           (if (empty? rest2)
+               (parse-ok e rest)
+               (parse-err))])])]))
 
-(define (parse-exp s) ;
+(define (parse-exp s)
   (type-case (ParseResult Exp) (parse-exp0 (list s))
     [(parse-err) (error 'parse-exp "Syntax error")]
-    [(parse-ok e rest) e]))
+    [(parse-ok e rest)
+     (if (empty? rest)
+         e
+         (error 'parse-exp "Syntax error"))]))
      
           
