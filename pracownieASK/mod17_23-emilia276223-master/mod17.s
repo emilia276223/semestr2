b1c797a0ce438fa1d13252b7101d538c20fa1924
Emilia <emilia.a.wis@gmail.com>
2023-05-11 14:31:06 +0200

    wersja bez jumpow

--- pracownieASK/mod17_23-emilia276223-master/mod17.s
+++ pracownieASK/mod17_23-emilia276223-master/mod17.s
@@ -22,11 +22,9 @@
 .LC2: .quad 0xF0F0F0F0F0F0F0F0
 
 mod17:
-		
-	mov %rdi, %rax
-	mov %rdi, %rsi
+	mov %rdi, %rcx
 	and .LC1, %rdi /*te co mam dodac */
-	and .LC2, %rax /*te co mam odjac */
+	and .LC2, %rcx /*te co mam odjac */
 	# nie bawie sie z pierwszym bitem bo to jest int a nie uint
 
 	# teraz z druga czescia
@@ -41,64 +39,47 @@ mod17:
 
 	mov %rdi, %r9
 	shr $8, %r9
-	lea (%r9, %rdi), %rcx/*w pierwszych 8 bitach mam 2 liczby ktore sa do zsumowania*/
+	lea (%r9, %rdi), %rax/*w pierwszych 8 bitach mam 2 liczby ktore sa do zsumowania*/
 
 
-	shr $4, %rax
+	shr $4, %rcx
 	# policzenie, ile mam odjac = zsumowanie 
-	mov %rax, %r9
+	mov %rcx, %r9
 	shr $32, %r9
-	add %r9, %rax /*w pierwszych 32 bitach mam liczby ktore sa do zsumowania */
+	add %r9, %rcx /*w pierwszych 32 bitach mam liczby ktore sa do zsumowania */
 
-	mov %rax, %r9
+	mov %rcx, %r9
 	shr $16, %r9
-	add %r9, %rax /*w pierwszych 16 bitach mam 4 liczby ktore sa do zsumowania */
+	add %r9, %rcx /*w pierwszych 16 bitach mam 4 liczby ktore sa do zsumowania */
 
-	mov %rax, %r9
+	mov %rcx, %r9
 	shr $8, %r9 
-	add %r9, %rax /*w pierwszych 8 bitach mam 2 liczby ktore sa do zsumowania, sumuje */
-
-	# and $0xFFF, %rax /*czyszcze stare bity*/
-
+	add %r9, %rcx /*w pierwszych 8 bitach mam 2 liczby ktore sa do zsumowania, sumuje */
 
 	# teraz mamy juz zsumowane liczby z tych bitow do dodania i do odjecia
-	and $0xFF, %rax
 	and $0xFF, %rcx
-	sub %rax, %rcx
+	and $0xFF, %rax
+	sub %rcx, %rax
 	# do tego miejsca 25 instrukcji (mam jezcze 10)
 
+	mov %rax, %rcx
+	and $0x800, %rcx
+	shr $11, %rcx
+	mov %rax, %rdi
+	and $0xF0, %rdi
+	shr $4, %rdi
+	and $0xF, %rax
+	sub %rdi, %rax
+	sub %rcx, %rax
+	mov %rax, %r10
+	and $0x100, %r10
+	shr $8, %r10
 
 
+	lea (%r10, %r10, 8), %rcx
+	lea (%rcx, %r10, 8), %rcx
+	add %rcx, %rax
 
-
-
-
-
-
-	# sbb %rdx, %rdx /* zapisujemy na pozniej czy wyszlo ujemne */
-
-	# do tego miejsca wynik jest poprawny, ale jeszcze nie jest to reszta (wynik miedzy 120 (78) a -120 (-78))
-
-	# mov %rcx, %rax
-	# sar $4, %rax
-	
-	# sub %rax, %rcx
-	# dodatkowo odejmuje 1 jesli byly ujemne / dodaje 1
-	# sub %rdx, %rcx
-
-	/*
-.L1:	sub $17, %rcx
-	cmp $0, %rcx
-	jge .L1
-.L2:	add $17, %rcx
-	cmp $0, %rcx
-	jl .L2
- */
-
-
-	mov %rcx, %rax
 	ret
-
     .size   mod17, .-mod17
 
-	# 18:00
